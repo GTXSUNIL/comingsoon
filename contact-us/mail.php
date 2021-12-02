db37a79123d880201b9957209a0f8346b0810022
GTXSUNIL <77480994+GTXSUNIL@users.noreply.github.com>
2021-12-02 11:14:47 +0530

    Update mail.php

--- contact-us/mail.php
+++ contact-us/mail.php
@@ -1,1 +1,21 @@
 
+<?php
+//get data from form  
+$name = $_POST['First Name'];
+$LastName = $_POST['Last Name'];
+$email= $_POST['E-mail'];
+$message= $_POST['Message'];
+$number = $_POST['Phone'];
+
+$to = "sunilsahu0577@gmail.com";
+
+$subject = "Mail From codeconia";
+$txt ="Name = ". $name . "\r\n Email = " . $email . "\r\n Message =" . $message ."\r\n Mobile number =". $number ."\r\n Last Name =".$Last Name;
+
+$headers = "From: noreply@unityenterprise.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:contact.html");
+?>
